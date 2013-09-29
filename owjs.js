fa2a08343ac6fc50095458e7c33cbefb1077e93f
Peter Magnusson <peter@birchroad.net>
2013-09-29 17:43:46 +0200

    Support promises or single callback in node style.
    * Added a .readFamily method to ease some things.
    * Added Mocha Tests
    * Updated README
    * Bumped version in package.json because of interface changes.

--- owjs.js
+++ owjs.js
@@ -1,14 +1,51 @@
 // based on https://github.com/benediktarnold/owfs
-var net = require('net');
+
+/*jshint latedef:nofunc, white:true, node:true, undef:true, unused:true */
+
+var net = require('net'),
+    Q = require('Q');
 
 var OW_READ =   2; // read from 1-wire bus
 var OW_WRITE =  3; // write to 1-wire bus
-var OW_DIR =    4; // list 1-wire bus
-var OW_PRESENT = 6; // Is the specified component recognized and known
+//UNUSED: var OW_DIR =    4; // list 1-wire bus
+//UNUSED: var OW_PRESENT = 6; // Is the specified component recognized and known
 var OW_DIRALL = 7; // list 1-wire bus, in one packet string
-var OW_GET =    8; // dirall or read depending on path
-var OW_DIRALLSLASH = 9; // dirall but with directory entries getting a trailing '/'
-var OW_GETSLASH = 10; // dirallslash or read depending on path
+//UNUSED: var OW_GET =    8; // dirall or read depending on path
+//UNUSED: var OW_DIRALLSLASH = 9; // dirall but with directory entries getting a trailing '/'
+//UNUSED: var OW_GETSLASH = 10; // dirallslash or read depending on path
+
+
+/**
+ * generate a node type callback call if it's set to a function
+ * otherwise return the promise
+ */
+function nodify(promise, callback) {
+    if (typeof callback === 'function') {
+        promise.then(function (result) {
+            return callback(null, result);
+        })
+        .catch(callback);
+
+        return;
+    }
+    else {
+        return promise;
+    }
+}
+
+
+// test device path for family id
+// internal
+function familyFilter(family) {
+    return function (path) {
+        if (path.substring(1, 3) === family) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+}
 
 
 function Client(options) {
@@ -19,13 +56,13 @@ function Client(options) {
     self.option = {
         host: 'localhost',
         port: 4304,
-    }
+    };
 
-// set options
-    for(var i in options) {
-        if(typeof(options[i]) == 'object') {
+    // set options
+    for (var i in options) {
+        if (typeof(options[i]) == 'object') {
             self.option[i] = {};
-            for(var j in options[i]) {
+            for (var j in options[i]) {
                 self.option[i][j] = options[i][j];
             }
         } else {
@@ -33,40 +70,47 @@ function Client(options) {
         }
     }
 
-// define procedures
-    self.htonl = function(n) {
+
+    // define procedures
+    self.htonl = function (n) {
         return [
             (n & 0xFF000000) >>> 24,
             (n & 0x00FF0000) >>> 16,
             (n & 0x0000FF00) >>>  8,
             (n & 0x000000FF) >>>  0,
         ];
-    }
+    };
 
-    self.ntohl = function(b) {
-      return ((0xff & b[0]) << 24) |
+
+    self.ntohl = function (b) {
+        return ((0xff & b[0]) << 24) |
              ((0xff & b[1]) << 16) |
              ((0xff & b[2]) << 8) |
              ((0xff & b[3]));
-    }
+    };
 
-    // send raw data to 1-wire and return result
-    self.send = function(path, value, type, callback, err) {
+
+    // send raw data to 1-wire and return responding
+    // message using a promise if not callback is provided
+    self.send = function (path, value, type, callback) {
+        var deferred = Q.defer();
         var socket = new net.Socket({ type: 'tcp4' });
         var messages = [];
 
-        socket.on('error', function(error) {
-            throw new Error(error);
+        socket.on('error', function (error) {
+            deferred.reject(new Error(error));
         });
 
-        socket.on('end', function() {
-            callback(messages);
+        //finished receiving
+        socket.on('end', function () {
+            deferred.resolve(messages);
         });
 
+        //receive data
         socket.on('data', function (data) {
-            var j=0, chunk = 4, header ={};
-            for (var i=0; i<24; i+=chunk) {
-                var temparray = data.slice(i,i+chunk);
+            var j = 0, chunk = 4, header = {};
+            for (var i = 0; i < 24; i += chunk) {
+                var temparray = data.slice(i, i + chunk);
                 var value = self.ntohl(temparray);
                 header[self.headers[j]] = value;
                 j++;
@@ -77,10 +121,14 @@ function Client(options) {
             });
         });
 
-        socket.connect(self.option.port, self.option.host, function() {
+        //send stuff
+        socket.connect(self.option.port, self.option.host, function () {
             var msg = [];
             path += '\x00';
-            value = (type == OW_WRITE) ? value.toString() + '\x00' : '' ;
+            if (type === OW_WRITE && (typeof value === 'undefined' || value === null)) {
+                throw new Error("Must have a value to write to " + path);
+            }
+            value = (type == OW_WRITE) ? value.toString() + '\x00' : '';
             // http://owfs.org/index.php?page=owserver-message-types
             msg = msg.concat(
                 self.htonl(0),
@@ -96,38 +144,95 @@ function Client(options) {
 
             socket.end(buf);
         });
-    }
+
+        return nodify(deferred.promise, callback);  
+    };
+
 
     // return array of dir
-    self.list = function(path, callback, error) {
-        self.send(path, null, OW_DIRALL, function(data) {
-            // if(data[0].header.ret < 0) return self.emit('error', data[0].header.ret);
-            if(data[0].header.ret < 0) return error(data[0].header.ret);
-            var str = data[0].payload;
-            str = str.substring(0, str.length - 1); // remove zero-char from end
-            callback(str.split(','));
-        });
-    }
+    self.list = function (path, callback) {
+        var promise;
+        if (path instanceof Array) {
+            promise = Q.all(path.map(self.list));
+        }
+        else {
+            promise = self.send(path, null, OW_DIRALL)
+            .then(function (messages) {
+                var message = messages[0];
+                if (message.header.ret < 0) {
+                    throw new Error(message.header.ret);
+                } 
+                var str = message.payload;
+                str = str.substring(0, str.length - 1); // remove zero-char from end
+                return str.split(',');
+            });
+        }
+
+        return nodify(promise, callback);
+    };
+
 
     // read value
-    self.read = function(path, callback, error) {
-        self.send(path, null, OW_READ, function(data) {
-            // if(data[0].header.ret < 0) return self.emit('error', data[0].header.ret);
-            if(data[0].header.ret < 0) return error(data[0].header.ret);
-            callback(data[0].payload);
-        });
-    }
+    self.read = function (path, callback) {
+        var promise;
+        
+        if (path instanceof Array) {
+            promise = Q.all(path.map(self.read));
+        }
+        else {
+            promise = self.send(path, null, OW_READ)
+            .then(gotMessages);
+        }
+
+        function gotMessages(messages) {
+            var message = messages[0];
+            if (message.header.ret < 0) {
+                throw new Error(message.header.ret);  
+            } 
+            return {path: path, value: message.payload};
+        }
+
+        return nodify(promise, callback);
+    };
+
 
     // write value
-    self.write = function(path, value, callback, error) {
-        self.send(path, value, OW_WRITE, function(data) {
-            console.log(data)
-            // if(data[0].header.ret < 0) return self.emit('error', data[0].header.ret);
-            if(data[0].header.ret < 0) return error(data[0].header.ret);
-            callback();
-        });
-    }
+    self.write = function (path, value, callback) {
+        return nodify(
+            self.send(path, value, OW_WRITE)
+            .then(function (messages) {
+                if (messages[0].header.ret < 0) {
+                    throw new Error(messages[0].header.ret);  
+                } 
+                return true;
+            }),
+            callback
+        );
+    };
 
+
+    // read a specific property from all devices in family
+    self.readFamily = function (family, property, callback) {
+        if (typeof family !== 'string') {
+            family = family.toString();
+        }
+        function filterDevices(devices) {
+            var filtered = devices.filter(familyFilter(family)).map(
+                function (element) {
+                    return element + '/' + property;
+                }
+            );
+
+            return self.read(filtered);
+        }
+
+        return nodify(
+            self.list('/').then(filterDevices),
+            callback);
+    };
+
+
+    
 }
 
 exports.Client = Client;
